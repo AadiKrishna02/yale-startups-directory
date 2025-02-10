454cee4842538a4896738715e89d11115e4ffa45
Aadi <165424350+AadiKrishna02@users.noreply.github.com>
2025-02-10 03:48:01 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,12 +6,12 @@ import { Menu, X } from 'lucide-react';
 
 const navItems = [
   { 
-    name: 'Submit', 
+    name: 'Submit Your Startup', 
     href: 'https://docs.google.com/forms/d/e/1FAIpQLSfeuaJP4vEFNQuGmkOE4wXpbEUJluGD9gO308-NbzbvduTekQ/viewform',
     isExternal: true
   },
   { 
-    name: 'About', 
+    name: 'About Us', 
     href: '/about',
     isExternal: false 
   },
