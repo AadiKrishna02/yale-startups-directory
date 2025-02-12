355088756acfecacd9eb465ba651bf38a6b917ef
Aadi <165424350+AadiKrishna02@users.noreply.github.com>
2025-02-11 20:31:07 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-// import { Analytics } from '@vercel/analytics/next';
+import Script from 'next/script';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -22,7 +22,18 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
         {children}
-{/*         <Analytics mode="production" />; */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0D5L3N27SE"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0D5L3N27SE');
+          `}
+        </Script>
       </body>
     </html>
   );
