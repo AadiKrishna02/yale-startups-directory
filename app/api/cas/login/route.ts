843d44dbf591823dd2901eb0c88de6b3497b4217
j4ck132 <77988751+j4ck132@users.noreply.github.com>
2025-03-24 03:00:09 -0400

    asdf

--- app/api/cas/login/route.ts
+++ app/api/cas/login/route.ts
@@ -6,6 +6,6 @@ export async function GET() {
     return NextResponse.json({ error: "Base URL is not defined" }, { status: 500 });
   }
   const serviceUrl = encodeURIComponent(`${baseUrl}/api/cas/callback`);
-  const casLoginUrl = `https://secure.its.yale.edu/cas/login?service=${serviceUrl}`;
+  const casLoginUrl = `https://secure6.its.yale.edu/cas/login?service=${serviceUrl}`;
   return NextResponse.redirect(casLoginUrl);
 }
