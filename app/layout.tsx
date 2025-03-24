e2c9bb6849422d11eedd550b90f96ebf6f8ec71c
j4ck132 <77988751+j4ck132@users.noreply.github.com>
2025-03-24 01:32:40 -0400

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-// app/layout.tsx
 import type { Metadata } from "next";
 import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext"; // adjust the path as needed
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,7 +23,9 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className={`${inter.variable} antialiased`}>
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-0D5L3N27SE"
           strategy="afterInteractive"
